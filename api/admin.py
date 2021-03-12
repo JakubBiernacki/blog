1337484ba5b763c3554419c2137e8c4f00d4bd3f
Jakub <mail>
2021-03-12 22:26:45 +0100

    make basic models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','user','tytul','data_utworzenia','zdjecia']
+    search_fields = ['tytul']
+    list_filter = ['user']
+
+@admin.register(Komentarz)
+class KomentarzAdmin(admin.ModelAdmin):
+    list_display = ['id','user','post','tresc','data_utworzenia','rodzic_id']
+    list_filter = ['post']
+
+    search_fields = ['tresc']
--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    tytul = models.CharField(max_length=255)
+    tresc = models.TextField()
+    zdjecia = models.ImageField(upload_to='post_img',null=True,blank=True,default=None)
+    data_utworzenia = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tytul
+
+class Komentarz(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rodzic = models.ForeignKey("self", on_delete=models.CASCADE,null=True,blank=True,default=None)
+    tresc = models.TextField(blank=False)
+    data_utworzenia = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tresc
